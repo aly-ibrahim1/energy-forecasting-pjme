27fade6bc736fa207f81857f20f2a8a6620887e0
aly-ibrahim1 <alyeltaayeb@gmail.com>
2025-08-29 02:18:57 +0300

    add fourier + moving avg plots, add residual/deterministic features functions

---
+++ src/functions.py
@@ -0,0 +1,95 @@
+
+from statsmodels.tsa.deterministic import DeterministicProcess, CalendarFourier
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+def make_deterministic_features(y, train_start=None, train_end=None,
+    order=2, fourier_order=5, model=LinearRegression(fit_intercept=False)):
+    """
+    Create deterministic (trend + Fourier seasonal) features
+    for a time series y (with a PeriodIndex or DatetimeIndex).
+
+    Parameters:
+        y : pd.Series
+            target time series with a proper time index.
+        order : int, default=2
+            polynomial trend order (1 = linear, 2 = quadratic).
+        fourier_order : int, default=5
+            number of Fourier harmonics for annual seasonality.
+        model : default=LinearRegression()
+
+    Returns:
+        dp : DeterministicProcess
+            The fitted deterministic process object (for forecasting later).
+        X_dp : pd.DataFrame
+            in-sample deterministic features aligned to y.
+    """
+    fourier_year = CalendarFourier(freq="A", order=fourier_order)
+    fourier_week = CalendarFourier(freq="W", order=3)
+
+    dp = DeterministicProcess(
+        index=y.index,
+        constant=True,
+        order=order,
+        seasonal=False,
+        additional_terms=[fourier_year, fourier_week],
+        drop=True,
+    )
+    X_dp = dp.in_sample()
+
+    # choose fit window
+    if (train_start is not None) and (train_end is not None):
+        X_dp_train = X_dp.loc[train_start:train_end]
+        y = y.loc[train_start:train_end]
+
+    # Fit trend model
+    model.fit(X_dp_train, y)
+
+    # fit and return fit-window outputs
+    y_hat_fit = pd.DataFrame(model.predict(X_dp_train), index=y.index)
+    resid_fit = y - y_hat_fit
+
+    return dp, X_dp, model, y_hat_fit, resid_fit, X_dp_train
+
+
+def make_residual_features(y, calendar_df, holidays, lags=[1,2,7,14,21,28], windows=[7,30,365]):
+    """
+        create lag and rolling features for residual modeling,
+        and combine with calendar/holiday features.
+
+        Parameters:
+            y : pd.Series
+                Target time series with proper time index.
+            calendar_df : pd.DataFrame
+                Calendar features (dow, is_weekend, month, etc.).
+            holiday_df : pd.DataFrame
+                Holiday features (is_holiday).
+            lags : list of int
+                lags (in periods) to include.
+            windows : list of int
+                rolling window sizes for mean/std.
+
+        Returns:
+            X : pd.DataFrame
+                feature matrix (lags, rolling, calendar, holiday).
+        """
+    X = pd.DataFrame(index=y.index)
+
+    # lags
+    for lag in lags:
+        X[f"lag_{lag}"] = y.shift(lag)
+
+    # rolling mean/std
+    for w in windows:
+        r = y.rolling(window=w)
+        X[f"roll{w}_mean"] = r.mean()
+        X[f"roll{w}_std"] = r.std()
+
+    # holidays
+    X['is_holiday'] = y.index.isin(holidays).astype(int)
+
+    # calendar
+    X = X.join(calendar_df)
+
+    return X
---
+++ src/plots.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from warnings import simplefilter
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
+
+simplefilter("ignore")
+
+# Set Matplotlib defaults
+sns.set(style="whitegrid")
+plt.rc("figure", autolayout=True, figsize=(11, 5))
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=16,
+    titlepad=10,
+)
+
+plot_params = dict(
+    color="0.75",
+    style=".-",
+    markeredgecolor="0.25",
+    markerfacecolor="0.25",
+    legend=False,
+)
+
+def seasonal_plot(X, y, period, freq, ax=None):
+    if ax is None:
+        _, ax = plt.subplot()
+    palette = sns.color_palette('husl', n_colors = X[period].nunique())
+    ax = sns.lineplot(
+        x=freq,
+        y=y,
+        hue=period,
+        data=X,
+        ci=False,
+        ax=ax,
+        palette=palette,
+        legend=False
+    )
+    ax.set_title(f"Seasonal Plot ({period}/{freq})")
+    for line, name in zip(ax.lines, X[period].unique()):
+        y_ = line.get_ydata()[-1]
+        ax.annotate(
+            name,
+            xy=(1, y_),
+            xytext=(6, 0),
+            color=line.get_color(),
+            xycoords=ax.get_yaxis_transform(),
+            textcoords="offset points",
+            size=14,
+            va="center",
+        )
+    return ax
+
+
+def plot_periodogram(ts, detrend='linear', ax=None):
+    from scipy.signal import periodogram
+    fs = pd.Timedelta("365D") / pd.Timedelta("1D")
+    freqencies, spectrum = periodogram(
+        ts,
+        fs=fs,
+        detrend=detrend,
+        window="boxcar",
+        scaling='spectrum',
+    )
+    if ax is None:
+        _, ax = plt.subplots()
+    ax.step(freqencies, spectrum, color="purple")
+    ax.set_xscale("log")
+    ax.set_xticks([1, 2, 4, 6, 12, 26, 52, 104])
+    ax.set_xticklabels(
+        [
+            "Annual (1)",
+            "Semiannual (2)",
+            "Quarterly (4)",
+            "Bimonthly (6)",
+            "Monthly (12)",
+            "Biweekly (26)",
+            "Weekly (52)",
+            "Semiweekly (104)",
+        ],
+        rotation=30,
+    )
+    ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+    ax.set_ylabel("Variance")
+    ax.set_title("Periodogram")
+    return ax
